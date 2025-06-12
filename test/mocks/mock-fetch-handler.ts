99d484b64f94ef3b022b8c90485784bed72b97ea
rebecca shoptaw <rebecca@rebeccashoptaw.dev>
2025-06-12 10:38:38 -0400

    Remove unused params from mock fetch handler

--- test/mocks/mock-fetch-handler.ts
+++ test/mocks/mock-fetch-handler.ts
@@ -1,26 +1,15 @@
 import type { FetchHandlerInterface } from '@internetarchive/fetch-handler-service/dist/src/fetch-handler-interface';
 
 export class MockFetchHandler implements FetchHandlerInterface {
-  async fetchApiResponse<T>(
-    url: string,
-    options?: {
-      includeCredentials?: boolean;
-      method?: string;
-      body?: BodyInit;
-      headers?: HeadersInit;
-    },
-  ): Promise<T> {
+  async fetchApiResponse<T>(): Promise<T> {
     return { success: true } as T;
   }
 
-  async fetchIAApiResponse<T>(
-    path: string,
-    options?: { includeCredentials?: boolean },
-  ): Promise<T> {
+  async fetchIAApiResponse<T>(): Promise<T> {
     return {} as T;
   }
 
-  async fetch(input: RequestInfo, init?: RequestInit): Promise<Response> {
+  async fetch(): Promise<Response> {
     return new Response();
   }
 }
